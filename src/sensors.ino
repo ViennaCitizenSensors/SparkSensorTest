ff6a58f706f11f7005fb49fd28c23b75bfb2e9a9
asterix555 <joky.es@gmail.com>
2015-04-01 16:31:30 +0200

    added licence information

--- src/sensors.ino
+++ src/sensors.ino
@@ -1,4 +1,37 @@
-// This #include statement was automatically added by the Spark IDE.
+/*
+ * sensors.ino
+ * A library for barometer
+ *
+ * Copyright (c) 2015 School of data
+ * Website    : https://schoolofdata.wordpress.com/
+ * Author     : Erwin Preuner
+ * Change Log :
+ *
+ * loovee 9-24-2014
+ * Change all int to short, all unsigned int to unsigned short to fit some 32bit system
+ *
+ * The MIT License (MIT)
+ *
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 #include "Barometer_BMP085.h"
 
 #define AIRQUALITY A0
